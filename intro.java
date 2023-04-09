4e4757c16aa88a150be790e029b23000ae02273d
hellprogmaker <60592559+hellprogmaker@users.noreply.github.com>
2023-04-09 14:33:58 +0300

    Add files via upload

---
+++ intro.java
@@ -0,0 +1,105 @@
+import java.util.Scanner; // импорт "библиотеки", как в Си #include <...>
+
+
+
+public class intro { // соответствует названию класса;
+
+    public static void main(String[] args) { // main-function
+        
+        /*
+
+        System.out.println("The 1st project"); //
+        System.out.print("sdfs");
+        System.out.println();
+        System.out.println();
+
+        */
+
+
+
+        /*
+
+        int age = 21; // создание переменных, также как в Си;
+        System.out.println(age);
+        age = 23;
+        System.out.print(age);
+
+        */
+
+
+
+        /*
+
+        int age = 12;
+        System.out.println("Age: " + age);
+
+        byte num = 127; // для этого типа данных 1 байт;
+        
+        */
+        
+        // float, double - работают также;
+        //float a1 = 123.123f;
+        //double a2 = 12.2312; // можно и так: a2 = 12.2312f;
+        //System.out.printf("a2 = %.2f", a2);
+
+        /* символьный тип
+        
+        char ch = '4';
+        System.out.println(ch);
+
+        */
+
+
+
+        /*
+
+        String name = "Alexander Ivan";
+        System.out.println(name);
+
+        boolean isFlag = true;
+        System.out.println(isFlag);
+
+        */
+
+
+
+        // Класс Scanner импортировали в шапке;
+        // Scanner способен считывать из консоли;
+        // System.in - ввод с консоли;
+        /*
+        Scanner object = new Scanner(System.in);
+        String str = object.nextLine(); // считать строку методом nextLine();
+        System.out.println("object: " + str);
+        //System.out.printf("object: %s\n", str);
+        */
+
+
+
+        /*
+
+        Scanner object = new Scanner(System.in);
+        int num = object.nextInt(); // nextInt() - метод для ввода чисел;
+        System.out.println("object: " + num);
+    
+        */
+
+
+
+        /*
+
+        int num1 = 50, num2 = 12, res;
+        res = num1 / num2;
+        System.out.println(res);
+        res += 7;
+        System.out.println(res);
+        res++;
+        System.out.println(res);
+
+        */
+
+
+
+        // условные конструкции - также как в Си; (if/else/else if)
+        
+    }
+}
