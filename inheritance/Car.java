f7d0100005a8667ec322c6e243c3de92f143d9a6
hellprogmaker <60592559+hellprogmaker@users.noreply.github.com>
2023-04-10 04:47:43 +0300

    Update Car.java

--- inheritance/Car.java
+++ inheritance/Car.java
@@ -1,5 +1,6 @@
-public class Car extends Transport {
+public class Car extends Transport implements ILights {
 
+	private boolean isOn; // включение света;
 	public Engine engine = new Engine(); // рабочий двигатель, car проехал 100 км.
 
 	public Car(double speed, double weight, String color) {
@@ -25,5 +26,17 @@ public class Car extends Transport {
 	public void ShowSpeed(double speed) {
 
 		System.out.println("Object speed: " + speed);
+	}
+
+	@Override
+	public void SetLight(boolean set) {
+
+		isOn = set;
+	}
+
+	@Override
+	public void BlinkLight() {
+
+		System.out.println("Light turned on.");
 	}
 }
