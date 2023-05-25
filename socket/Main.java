183fa2e5064852ca15abb89832e50cd1504046e8
hellprogmaker <60592559+hellprogmaker@users.noreply.github.com>
2023-05-25 20:32:50 +0300

    Update Main.java

--- socket/Main.java
+++ socket/Main.java
@@ -1,20 +1,51 @@
-import java.io.InputStream;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
+
+    static final String DataBaseFile = "database.txt";
+    static final String EquationFile = "equation.txt";
+
     public static void main(String[] args) throws Exception {
 
-        ServerSocket serverSocket = new ServerSocket(8081);
-        Socket input = serverSocket.accept();
+        File DataBase = new File(DataBaseFile);
+        File Equation = new File(EquationFile);
 
+//while(true) {
+
+        ServerSocket serverSocket = new ServerSocket(1111);
+        Socket input = serverSocket.accept();
         Scanner scanner = new Scanner(input.getInputStream());
-        while(scanner.hasNext())
-            System.out.println(scanner.nextLine());
+
+        String CapacityBuffer = scanner.nextLine();
+        String buffer[] = CapacityBuffer.split(" ");
+        record(CapacityBuffer + " " + scanner.hashCode(), DataBase, true);
+        record(buffer[1], Equation, false);
+
+        Process process = Runtime.getRuntime().exec(new String[]{"cmd", "/c", "a"},
+                null, new File("C:\\Users\\Alexander\\IdeaProjects\\Socket"));
 
         scanner.close();
         input.close();
         serverSocket.close();
+//}
+    }
+
+    public static void record(String string, File file, boolean status) throws FileNotFoundException {
+
+        FileOutputStream fileOutputStream = new FileOutputStream(file, status);
+        byte[] buffer = string.getBytes();
+
+        try {
+
+            fileOutputStream.write(buffer, 0, buffer.length);
+            fileOutputStream.write('\n');
+        } catch (IOException e) {
+
+            throw new RuntimeException(e);
+        }
     }
 }
