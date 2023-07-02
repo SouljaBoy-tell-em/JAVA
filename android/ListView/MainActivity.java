3427e7c81e8d3c84a9f3df54dc3a624edb9b0de3
hellprogmaker <60592559+hellprogmaker@users.noreply.github.com>
2023-07-02 18:09:42 +0300

    Update MainActivity.java

--- android/ListView/MainActivity.java
+++ android/ListView/MainActivity.java
@@ -4,23 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        ArrayList<Product> productsArrayList = new ArrayList<Product>();
+        productsArrayList.add(new Product("New Balance 327", 12000, R.drawable.newbalance584));
+        productsArrayList.add(new Product("New Balance 550", 19000, R.drawable.newbalance584));
 
-//        ConstraintLayout[] productsLayout = {(new Product("New Balance 584", 12000, R.drawable.newbalance584)).OptionsProduct(this),
-//                                             (new Product("New Balance 327", 19000, R.drawable.newbalance327)).OptionsProduct(this)};
-//
-//        ListView listProducts = new ListView(this);
-//        ArrayAdapter<ConstraintLayout> listProductsAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, productsLayout);
-//        listProducts.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-//        listProducts.setAdapter(listProductsAdapter);
-//        setContentView(listProducts);
+        ListView productsList = findViewById(R.id.listView);
+        ProductAdapter productAdapter = new ProductAdapter(this, R.layout.items, productsArrayList);
+        productsList.setAdapter(productAdapter);
     }
 }
