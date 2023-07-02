9e62344053d08eb3edbaaa0b61d2a232d136edeb
hellprogmaker <60592559+hellprogmaker@users.noreply.github.com>
2023-07-02 18:09:59 +0300

    Update Product.java

--- android/ListView/Product.java
+++ android/ListView/Product.java
@@ -9,6 +9,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.Constraints;
 
 public class Product {
 
@@ -21,43 +22,6 @@ public class Product {
         this.name             =             name;
         this.price            =            price;
         this.resourceDrawable = resourceDrawable;
-    }
-
-    public ConstraintLayout OptionsProduct(Context context) {
-
-        ConstraintLayout constraintLayout = new ConstraintLayout(context);
-
-
-        ImageView imageProduct = new ImageView(context);
-        imageProduct.setId(View.generateViewId());
-        imageProduct.setImageResource(this.resourceDrawable);
-        ConstraintLayout.LayoutParams imageProductParams = new ConstraintLayout.LayoutParams(200, 200);
-        imageProductParams.topToTop = ConstraintLayout.LayoutParams.PARENT_ID;
-        imageProductParams.bottomToBottom = ConstraintLayout.LayoutParams.PARENT_ID;
-        imageProductParams.leftToLeft = ConstraintLayout.LayoutParams.PARENT_ID;
-        constraintLayout.addView(imageProduct, imageProductParams);
-
-
-        TextView nameText = new TextView(context);
-        nameText.setId(View.generateViewId());
-        nameText.setText(this.name);
-        ConstraintLayout.LayoutParams nameTextParams = new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        nameTextParams.topToTop = imageProduct.getId();
-        nameTextParams.leftToRight = imageProduct.getId();
-
-
-        TextView priceText = new TextView(context);
-        priceText.setText("PRICE: " + this.price + "$");
-        ConstraintLayout.LayoutParams priceTextParams = new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        priceTextParams.topToBottom = nameText.getId();
-        priceTextParams.leftToRight = imageProduct.getId();
-
-        constraintLayout.addView(imageProduct, imageProductParams);
-        constraintLayout.addView(nameText, nameTextParams);
-        constraintLayout.addView(priceText, priceTextParams);
-
-        return constraintLayout;
-
     }
 
     public String getName()  { return this.name;}
