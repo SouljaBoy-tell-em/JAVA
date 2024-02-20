22765c283130b3a6eca60bb9ea4349adb54124f1
hellprogmaker <60592559+hellprogmaker@users.noreply.github.com>
2024-02-20 20:22:33 +0300

    added jar file after compilation

--- SPRING/ASYNC MESSAGES/RabbitMQ/src/main/java/com/example/RabbitMQReceiver.java
+++ SPRING/ASYNC MESSAGES/RabbitMQ/src/main/java/com/example/RabbitMQReceiver.java
@@ -1,14 +1,9 @@
 package com.example;
 
 
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Component
