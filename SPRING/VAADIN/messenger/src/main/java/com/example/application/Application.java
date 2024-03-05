81cacafed50fefa42f931420058a26ecded58fe4
hellprogmaker <60592559+hellprogmaker@users.noreply.github.com>
2024-03-05 12:22:49 +0300

    updated vaadin project

--- SPRING/VAADIN/messenger/src/main/java/com/example/application/Application.java
+++ SPRING/VAADIN/messenger/src/main/java/com/example/application/Application.java
@@ -1,13 +1,18 @@
 package com.example.application;
 
-
 import com.vaadin.flow.component.dependency.NpmPackage;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
 @SpringBootApplication
 @Theme(value = "todo")
 @NpmPackage(value = "line-awesome", version = "1.3.0")
